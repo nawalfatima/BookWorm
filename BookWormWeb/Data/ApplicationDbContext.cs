24e761cd388a5df914a5f91a93bc4208a11b0fee
murtajiz110 <murtajiz110@yahoo.com>
2023-04-29 15:09:29 +0500

    Add project files.

---
+++ BookWormWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BookWormWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWormWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Horror", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Romance", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Fantasy", DisplayOrder = 5 }
+                );
+        }
+        public DbSet<CoverType> CoverTypes { get; set; }
+
+    }
+}
---
+++ BookWormWeb/Models/CoverType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BookWormWeb.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "CoverType")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
